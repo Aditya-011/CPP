d4bf5b26cedf5046866df68f7e4a7d33ba7182ef
Aditya-011 <adihgr18@gmail.com>
2021-05-14 14:14:03 +0530

    1506A

---
+++ Codeforces/1506A.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+const int M = 1e9 + 7;
+ll mod(ll x)
+{
+    return (x % M + M) % M;
+}
+
+ll mul(ll a, ll b)
+{
+    return mod((mod(a) * mod(b)));
+}
+void solve()
+{
+    ll m, n, k, x, y;
+    cin >> m >> n >> k;
+    if (k % m == 0)
+    {
+        y = k / m;
+        x = m;
+    }
+    else
+    {
+        y = k / m + 1;
+        x = k % m;
+    }
+    cout << n * (x - 1) + y << "\n";
+    return;
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cout << fixed;
+    cout << setprecision(10);
+    //        freopen("timber_input.txt", "r", stdin);
+    //        freopen("timber_output.txt", "w", stdout);
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
