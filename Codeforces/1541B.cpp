810c22282e71c7e8638ab209343c4029225817c8
Aditya-011 <adihgr18@gmail.com>
2021-07-13 20:20:09 +0530

    1541B

--- Codeforces/1541B.cpp
+++ Codeforces/1541B.cpp
@@ -1,39 +1,62 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
 using namespace std;
-int main()
-{
-    int t;
-    cin>>t;
-    while (t--)
-    {
-        long long n;
-        cin>>n;
-        vector<pair<long long,int>>arr;
-        for (long long i = 0; i < n; i++)
-        {
-            long long tmp ;
-            cin>>tmp;
 
-            arr.push_back(make_pair(tmp,i+1));
-        }
-        long long count=0,maxSum = n+n-1;
-        sort(arr.begin(),arr.end());
-        for (long long i = 0; i < n; i++)
+const int M = 1e9 + 7;
+ll mod(ll x)
+{
+    return (x % M + M) % M;
+}
+
+ll mul(ll a, ll b)
+{
+    return mod((mod(a) * mod(b)));
+}
+void solve()
+{
+    ll n;
+    cin >> n;
+    int count = 0;
+    vector<pair<ll, ll>> arr;
+    for (int i = 0; i < n; i++)
+    {
+        ll tmp;
+        cin >> tmp;
+        arr.push_back(make_pair(tmp, i));
+    }
+    sort(arr.begin(), arr.end());
+    for (ll i = 0; i < n - 1; i++)
+    {
+        for (ll j = i + 1; j < n; j++)
         {
-            for (long long j = 0; j < n; j++)
+            if ((arr[i].first * arr[j].first) == arr[i].second + arr[j].second + 2)
+                count++;
+            if ((arr[i].first * arr[j].first) > (2 * n))
             {
-            if((arr[i].first*arr[j].first)>=maxSum)
-            {
-                n = i+1;
                 break;
             }
-            else if((arr[i].first*arr[j].first)==(arr[i].second + arr[j].second))
-            count++;
-            }
-            
         }
-        
-        
     }
-    
+
+    cout << count << "\n";
+    return;
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cout << fixed;
+    cout << setprecision(10);
+    //        freopen("timber_input.txt", "r", stdin);
+    //        freopen("timber_output.txt", "w", stdout);
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
 }
