2d2e10662c5ee440cfa3b2ad910b476dd8d7522d
Aditya-011 <adihgr18@gmail.com>
2022-06-23 01:09:55 +0530

    the celebrity problem #2

--- practice/the_celebrity_problem.cpp
+++ practice/the_celebrity_problem.cpp
@@ -1,6 +1,6 @@
 // { Driver Code Starts
 //Initial template for C++
-
+/*
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -13,39 +13,139 @@ class Solution
     //Function to find if there is a celebrity in the party or not.
     int celebrity(vector<vector<int> >& M, int n) 
     {
-        // code here 
-    int sol=-1;
-    for(int i = 0 ;i<n;i++)
+    stack<int>st;
+    for(int i=0;i<n;i++)
+    st.push(i);
+    int count =0;
+    while(st.size()!=1)
     {
-        bool flag =0;
-        for(int j =0;j<n;j++)
-        {
-            if(M[i][j] == 1 )
-            {flag =1;
-            break;}
-        }
-        if(!flag)
-        
-        {
-            sol = i;
-            break;
-        }
+        int a = st.top();
+        st.pop();
+        int b = st.top();
+        st.pop();
+        if(M[a][b])
+        st.push(b);
+       else
+        st.push(a);
     }
-    int res=-1;
-   
-        int sum = 0;
-        for(int j =0;j<n;j++)
+    if(st.empty())
+    return -1;
+    int sum =0;
+    bool flag = 0;
+    for(int i=0;i<n;i++)
+    sum+= M[st.top()][i];
+    if(sum ==0)
+    {
+        for(int i=0;i<n;i++)
         {
-            sum += M[j][sol];
-            
+           sum+= M[i][st.top()] ;
         }
-        if(sum == n-1 )
-        {
-            res = sol;
-           
-        }
+        if(sum == n-1)
+        return st.top();
+    }
+    else return -1;
+      return st.top();
     
-    return res;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > M( n , vector<int> (n, 0));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cin>>M[i][j];
+            }
+        }
+        Solution ob;
+        cout<<ob.celebrity(M,n)<<endl;
+
+    }
+}
+  // } Driver Code Ends*/
+  //Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution 
+{
+    private:
+    bool knows(vector<vector<int> >& M, int a, int b, int n) {
+        if(M[a][b] == 1)
+            return true;
+        else
+            return false;
+    }
+    public:
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& M, int n) 
+    {
+        stack<int> s;
+        //step1: push all element in stack
+        for(int i=0; i<n; i++) {
+            s.push(i);
+        }   
+        
+        //step2: get 2 elements and copare them
+        
+        while(s.size() > 1) {
+            
+            int a = s.top();
+            s.pop();
+            
+            int b = s.top();
+            s.pop();
+            
+            if(knows(M,a,b,n)){
+                s.push(b);
+            }
+            else
+            {
+                s.push(a);
+            }
+        }
+        int ans = s.top();
+        //step3: single element in stack is potential celeb
+        //so verify it
+        
+        int zeroCount = 0;
+        
+        for(int i=0; i<n; i++) {
+            if(M[ans][i] == 0)
+                zeroCount++;
+        }
+        
+        //all zeroes
+        if(zeroCount != n)
+            return -1;
+        
+        //column check
+        int oneCount = 0;
+        
+        for(int i=0; i<n; i++) {
+            if(M[i][ans] == 1)
+                oneCount++;
+        }
+        
+        if(oneCount != n-1)
+            return -1;
+        
+        return ans;
+        
     }
 };
 
